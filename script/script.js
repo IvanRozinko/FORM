4832c5567937074cf345900e553ed83a386a0c83
Ivan Rozinko <ivanrozya@gmail.com>
2019-04-30 20:34:40 +0300

    commit after 1st review by Sergei Ushakov

--- script/script.js
+++ script/script.js
@@ -4,14 +4,15 @@ $(document).ready(function () {
     //connect slider plugin
     $(".slide").slide();
 
-
     /**
      * Checking is all inputs are valid, if yes - opening next form
      * @returns {boolean}
      */
     const email = document.getElementById("email");
     const pass = document.getElementById("pass");
+
     $("#submit").on("click", function () {
+
         if (isValid(email, regExps[0].regExp) &&
              isValid(pass, regExps[1].regExp) &&
               isAgree.checked)
@@ -24,22 +25,23 @@ $(document).ready(function () {
         }
     });
 });
-
 const regExps =
     [
         {name: "email", regExp: /.+@.+\..+/},
-        {name: "pass", regExp: /^[\w\d]{8,16}$/},
+        {name: "pass", regExp: /^[\w]{8,16}$/},
         {name: "name", regExp: /^[A-Za-z]+$/},
         {name: "age", regExp: /^[0-9]{1,2}$/}
     ];
 
 const validateInputs = document.getElementsByClassName("validateInput");
+
 Array.from(validateInputs).forEach((input) => {
     input.addEventListener("focusout", function () {
         const inputID = this.id;
         let obj = regExps.find(item => {
             return item.name === inputID
         });
+
         if (!isValid(this, obj.regExp)) {
             this.className = "invalid";
             this.removeEventListener("focusout", () => {
@@ -54,7 +56,6 @@ Array.from(validateInputs).forEach((input) => {
 //checkbox validation
 const isAgree = document.getElementById("agree");
 
-
 /**
  * Checking is string from input matches the regular expression and returning boolean answer
  * @param input
@@ -64,3 +65,4 @@ const isAgree = document.getElementById("agree");
 function isValid(input, regExp) {
     return regExp.test(input.value);
 }
+
